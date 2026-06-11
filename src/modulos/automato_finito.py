f6b312e00ccd4759de5a9104aec0f8aa7af9c79b
Vitor Cesar <vitorcesarglima05@gmail.com>
2026-06-11 09:52:38 -0300

    refactor: ajuste de codigo AutomatoFinito

--- src/modulos/automato_finito.py
+++ src/modulos/automato_finito.py
@@ -1,6 +1,6 @@
 class AutomatoFinito:
 
-    def _init_(self, estados, estado_inicial, estados_finais, transicoes):
+    def __init__(self, estados, estado_inicial, estados_finais, transicoes):
         self.estados = estados
         self.estado_inicial = estado_inicial
         self.estados_finais = estados_finais
@@ -9,13 +9,21 @@ class AutomatoFinito:
     def executar(self, cadeia):
         estado_atual = self.estado_inicial
         historico = [estado_atual]
+        passos = []
 
         for simbolo in cadeia:
             chave = (estado_atual, simbolo)
+
             if chave not in self.transicoes:
-                return False, historico
-            estado_atual = self.transicoes[chave]
+                passos.append(f"Não existe transição: {estado_atual} --{simbolo}--> ?")
+                return False, historico, passos
+
+            proximo_estado = self.transicoes[chave]
+
+            passos.append(f"{estado_atual} --{simbolo}--> {proximo_estado}")
+
+            estado_atual = proximo_estado
             historico.append(estado_atual)
 
         aceita = estado_atual in self.estados_finais
-        return aceita, historico
+        return aceita, historico, passos
